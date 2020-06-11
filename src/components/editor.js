2065800f211ca2d405d0ec6e4ea7ee7cd66748b6
Ananya <ananyaganesh01@gmail.com>
2020-06-11 19:33:40 +0530

    Added a pdf generator

--- src/components/editor.js
+++ src/components/editor.js
@@ -2,19 +2,24 @@ import React from 'react';
 import SplitPane, { Pane } from 'react-split-pane';
 import './editor.css';
 import { Tabs } from 'antd';
+import Pdf from "react-to-pdf";
 
 const { TabPane } = Tabs;
+const ref = React.createRef();
 
 const Editor = () => {
     let small = 480;
     return(
         <div>
-        <h1> This is the editor </h1>
+        <Pdf targetRef={ref} filename="code-example.pdf">
+            {({ toPdf }) => <button onClick={toPdf} style={{float:"right"}}>Generate Pdf</button>}
+        </Pdf>
+        <h1> Online Groff editor </h1>
 
         {window.innerWidth > small ? (
             <SplitPane split="vertical"  defaultSize={600} primary="second" >
                 <div initialSize="50%">Groff </div>
-                <div initialSize="50%">Preview </div>
+                <div initialSize="50%" ref={ref}>Preview of Groff </div>
             </SplitPane>
         ): (
             <Tabs type="card">
