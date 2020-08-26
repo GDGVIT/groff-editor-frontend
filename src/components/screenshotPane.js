59124d05bb656a8a893f1d026dfa495a11ad599f
Adictya <aditya.26101@gmail.com>
2020-08-26 13:07:12 +0530

    Login: Added Interactability to Login Screenshots

--- src/components/screenshotPane.js
+++ src/components/screenshotPane.js
@@ -5,33 +5,70 @@ import Screenshot2 from "../assets/Screenshot.png";
 
 class screenshotPane extends React.Component {
 	state = {
-		curImgIndex: 1,
-		curImg: "Screenshot1",
-		curText: "See Live Groff Preview as you type your code",
+		curImgIndex: 0,
+		DisplayArray: [
+			{
+				curImg: Screenshot1,
+				curText: "See Live Groff Preview as you type your code",
+			},
+			{
+				curImg: Screenshot2,
+				curText: "Cloud storage for all your documents",
+			},
+		],
 	};
-	changeImg = () => {
-		if (this.state.curImg < 2) {
-			const updatedImg = this.state.curImg + 1;
-			this.setState({ curImg: updatedImg });
+	NextImg = () => {
+		if (this.state.curImgIndex < 1) {
+			const updatedImg = this.state.curImgIndex + 1;
+			this.setState({ curImgIndex: updatedImg });
+		} else {
+			this.setState({ curImgIndex: 0 });
+		}
+	};
+	PrevImg = () => {
+		if (this.state.curImgIndex > 0) {
+			const updatedImg = this.state.curImgIndex - 1;
+			this.setState({ curImgIndex: updatedImg });
+		} else {
+			this.setState({ curImgIndex: 1 });
 		}
 	};
 	render() {
 		return (
 			<div className={classes.screenshotPane}>
 				<div className={classes.MainContainer}>
-					<div className={classes.prev}>&#10094;</div>
-					<div className={classes.next}>&#10095;</div>
+					<div
+						className={classes.prev}
+						onClick={() => this.PrevImg()}
+					>
+						&#10094;
+					</div>
+					<div
+						className={classes.next}
+						onClick={() => this.NextImg()}
+					>
+						&#10095;
+					</div>
 					<div className={classes.slideshowContainer}>
 						<div className={classes.mySlides}>
 							<div className={classes.numbertext}>1 / 3</div>
 							<img
-								src={Screenshot1}
-								style={{ "max-width": "100%" }}
+								src={
+									this.state.DisplayArray[
+										this.state.curImgIndex
+									].curImg
+								}
+								style={{ maxWidth: "100%" }}
 								alt="Showcase"
 							/>
 						</div>
 					</div>
-					<div className={classes.text}>{this.state.curText}</div>
+					<div className={classes.text}>
+						{
+							this.state.DisplayArray[this.state.curImgIndex]
+								.curText
+						}
+					</div>
 				</div>
 			</div>
 		);
--- src/containers/login.js
+++ src/containers/login.js
@@ -1,13 +1,8 @@
 import React from "react";
 import LoginPane from "../components/loginPane";
 import ScreenshotPane from "../components/screenshotPane.js";
-import classes from "../components/screenshotPane.module.css";
 
 const Login = () => {
-	const style = {
-		backgroundColor: "#E79870",
-		display: "flex",
-	};
 	return (
 		<div className="LoginPage">
 			<LoginPane></LoginPane>
