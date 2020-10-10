65b12cb9c0133e734c215828bf27b94ed1fff6ef
Adictya <aditya.26101@gmail.com>
2020-10-10 23:22:39 +0530

    Test: Https

--- src/containers/editor.js
+++ src/containers/editor.js
@@ -14,7 +14,7 @@ import { useTheme } from "../context/ThemeContext";
 import options from "../options";
 import socketIOClient from "socket.io-client";
 
-const client = socketIOClient("http://groff.dscvit.com/api", {
+const client = socketIOClient("https://groff.dscvit.com/api", {
 	transports: ["websocket"],
 	path: "/api/socket.io"
 });
