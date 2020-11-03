b044856198974e2dc5fbc062e4ff3bf297a8e94b
adictya <aditya.26101@gmail.com>
2020-11-03 18:30:06 +0530

    Home: Added Template Previews

--- src/components/Documents/Document/templateDocument.js
+++ src/components/Documents/Document/templateDocument.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 
 const TemplateDocument = (props) => {
 	let History = useHistory();
-	console.log('CONTENTTT', props.title)
+	console.log("CONTENTTT", props.title);
 	return (
 		<MyContext.Consumer>
 			{(context) => {
@@ -13,23 +13,31 @@ const TemplateDocument = (props) => {
 					<div
 						className={classes.NewDocumentContainer}
 						onClick={async () => {
-							const newId = await context.TDocumentHandler(props.content, props.title);
+							const newId = await context.TDocumentHandler(
+								props.content,
+								props.title
+							);
 							History.push("/editor/" + newId);
 						}}
 						id="templatedocument"
 					>
-                    <div className={classes.DocumentPreview}></div>
-                    <div className={classes.DocumentDetails} id="docdeets">
-                        <div className={classes.tooltip}>
-                            <div className={classes.DocumentName}>
-                                {props.heading}
-                            </div>
-                            <span className={classes.tooltiptext}>
-                                {props.heading}
-                            </span>
-                        </div>
-                    </div>
-				</div>
+						<div
+							className={classes.DocumentPreview}
+							style={{
+								backgroundImage: "url(" + props.bgImg + ")",
+							}}
+						></div>
+						<div className={classes.DocumentDetails} id="docdeets">
+							<div className={classes.tooltip}>
+								<div className={classes.DocumentName}>
+									{props.heading}
+								</div>
+								<span className={classes.tooltiptext}>
+									{props.heading}
+								</span>
+							</div>
+						</div>
+					</div>
 				);
 			}}
 		</MyContext.Consumer>
